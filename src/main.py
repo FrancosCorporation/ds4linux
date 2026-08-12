cba08de6fdf03cd477fe569dbb749a3b5c3367d7
Seu Nome <seu-email@exemplo.com>
2026-08-11 23:40:27 -0300

    fix: import QLocalServer from QtNetwork instead of QtCore

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import Qt, QTimer, QLocalServer, QLocalSocket
+from PySide6.QtCore import Qt
+from PySide6.QtNetwork import QLocalServer, QLocalSocket
 import sys
 import signal
 import logging
